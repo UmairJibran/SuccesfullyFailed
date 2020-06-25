6253660655c43ff9e5faac372647521e49281e0e
Umair Jibran <umairjibran7@gmail.com>
2020-06-25 17:44:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let score = 85;
+console.log("Your Score: "+ score);
+let output = "You ";
+if(score <= 85){output+= "Failed";}
+if(score >= 85){output+= "Passed";}
+output += " the Test";
+console.log(output);
+console.log("Required Score: 85%");
